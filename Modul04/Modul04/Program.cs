962dafe4aa59cb0cf43c54fd2a3898e039d09199
Лилия Великова <lvelikova84@abv.bg>
2015-11-16 13:19:12 +0200

    Модул 04, Видео 02 - Примери с for

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -12,17 +12,30 @@ namespace Modul04
 //				Console.WriteLine ( i.ToString () );
 //			}
 
-			for ( int i = 0; i < 5 ; i += 2)
-			{
-				//Код на цикъла
-				Console.WriteLine ( i.ToString () );
-			}
+//			for ( int i = 0; i < 5 ; i += 2)
+//			{
+//				//Код на цикъла
+//				Console.WriteLine ( i.ToString () );
+//			}
 
 //			for ( int i = 5; i > 0 ; i--)
 //			{
 //				//Код на цикъла
 //				Console.WriteLine ( i.ToString () );
 //			}
+
+			string [] _table = { "Иван", "Гошо", "Гергана"};
+			
+			// Антетка на таблица
+			Console.WriteLine ("Име");
+			Console.WriteLine ( "==========" );
+
+			// Тяло на таблица
+			for ( int i = 0; i < _table.Length ; i ++)
+			{
+				//Код на цикъла
+				Console.WriteLine ( _table [i] );
+			}
 		}
 	}
 }

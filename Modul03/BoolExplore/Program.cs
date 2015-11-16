6cbfbac8dc588958ab8c7de99ae5bf153eb3cd35
Лилия Великова <lvelikova84@abv.bg>
2015-11-16 07:55:58 +0200

    Модул 03, Видео 06 - Работа с условни оператори if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -12,17 +12,35 @@ namespace BoolExplore
 
 			int[] _i = new int[] { 4, 3, 2 };
 			int _ui = Convert.ToInt32 (Console.ReadLine());
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
 
-			bool _exp01 = _check01 && _check02;		// Логическо и "&&"
-			bool _exp02 = _check01 || _check02; 	// Логическо или "||"
-			bool _exp03 =!_check01;					// Отрицание "!"
-
-			Console.WriteLine ("Логическо И: " + _exp01.ToString());
-			Console.WriteLine ("Логическо ИЛИ: " + _exp02.ToString());
-			Console.WriteLine ("Отрицание на " + _check01.ToString() + ": " + _exp03 );
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool _exp01 = _check01 && _check02;		// Логическо и "&&"
+//			bool _exp02 = _check01 || _check02; 	// Логическо или "||"
+//			bool _exp03 =!_check01;					// Отрицание "!"
+//
+//			Console.WriteLine ("Логическо И: " + _exp01.ToString());
+//			Console.WriteLine ("Логическо ИЛИ: " + _exp02.ToString());
+//			Console.WriteLine ("Отрицание на " + _check01.ToString() + ": " + _exp03 );
 			//Console.WriteLine (_i [_ui - 1]);
+
+			if (_ui>0 && _ui<=_i.Length)
+			{
+				Console.WriteLine (_i [_ui - 1]);
+			}
+//			else 
+//			{
+//				Console.WriteLine ("Невалиден индекс \n\n\n");
+//			}
+			if (_ui>_i.Length)
+			{
+				Console.WriteLine ("Индекса е прекалено голям\n\n\n");
+			}
+			if (_ui<=0)
+			{
+				Console.WriteLine ("Индекса е прекалено малък\n\n\n");
+			}
 		}
 	}
 }

d5a77bb65db4598bb0bbe5802d990e27ef4b4c4c
Лилия Великова <lvelikova84@abv.bg>
2015-11-16 12:50:15 +0200

    Модул 04, Видео 01 - Цикъл for

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+//			for ( int i = 0; i < 5 ; i++)
+//			{
+//				//Код на цикъла
+//				Console.WriteLine ( i.ToString () );
+//			}
+
+			for ( int i = 0; i < 5 ; i += 2)
+			{
+				//Код на цикъла
+				Console.WriteLine ( i.ToString () );
+			}
+
+//			for ( int i = 5; i > 0 ; i--)
+//			{
+//				//Код на цикъла
+//				Console.WriteLine ( i.ToString () );
+//			}
+		}
+	}
+}

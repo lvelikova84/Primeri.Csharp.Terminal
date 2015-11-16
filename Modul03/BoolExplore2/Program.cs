5debf46b70edd48b599dabde5ef3ab60045de25b
Лилия Великова <lvelikova84@abv.bg>
2015-11-16 11:18:58 +0200

    Модул 03, Видео 10 - Улавяне на грешки с оператора try catch

--- Modul03/BoolExplore2/Program.cs
+++ Modul03/BoolExplore2/Program.cs
@@ -16,39 +16,50 @@ namespace BoolExplore2
 
 			bool _check01 = int.TryParse(_input,out _ui);		
 
+			try
+			{
+				Console.Write("Елемент " + _ui.ToString() + " от масива е: ");
+				Console.WriteLine (_i [_ui - 1] );
+			} 
+			catch 	{
+				Console.WriteLine ("<недефиниран елемент>");
+					}
+
+// 			Използване на условно присвояване
 //			_ui = (_ui <= _i.Length) ? _ui : 3;
 //			_ui = (_ui > 0) ? _ui : 1;
 
+//			Използване на switch
+//			switch (_ui) {
+//			case 1:
+//				{
+//					Console.Write ("1-ви елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//
+//			case 2:
+//				{
+//					Console.Write ("2-ри елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//
+//			case 3:
+//				{
+//					Console.Write ("3-ти елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//
+//			default:
+//				{
+//					Console.Write ("В масива няма стойност за този индекс.\nопитайите с индекс между 1 и 3.\n\n");
+//					break;
+//				}
+//						}
 
-			switch (_ui) {
-			case 1:
-				{
-					Console.Write ("1-ви елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-
-			case 2:
-				{
-					Console.Write ("2-ри елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-
-			case 3:
-				{
-					Console.Write ("3-ти елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-
-			default:
-				{
-					Console.Write ("В масива няма стойност за този индекс.\nопитайите с индекс между 1 и 3.\n\n");
-					break;
-				}
-						}
-
+//			използване на условен оператор
 //			if (_check01)  {
 //				Console.WriteLine (_i [_ui - 1]);
 //			}

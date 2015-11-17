044c13baa81afdab26879d6c716941bf45d285f8
Лилия Великова <lvelikova84@abv.bg>
2015-11-17 09:42:12 +0200

    Модул 04, Видео 06 - Примери с do ... while

---
+++ Modul04/ListExplore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Дефиниции
+			string _userinput = "";
+			//List
+
+
+			// Потребителски вход
+			do {
+				Console.Write ("? ");
+				_userinput = Console.ReadLine();
+				// Добавяне на стойности в листа
+
+
+				// Преглед на листа
+
+
+				// Размер на листа
+
+			} while (_userinput != "exit");
+
+		}
+	}
+}
